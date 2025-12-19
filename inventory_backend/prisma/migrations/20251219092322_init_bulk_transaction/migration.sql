0136a22fbed209338cc907cbebd5b979edf93c92
RefoFebrian <muhammadrefof@gmail.com>
2025-12-19 16:29:30 +0700

    Refo: Add auto restart service setiap server restart

--- inventory_backend/prisma/migrations/20251219092322_init_bulk_transaction/migration.sql
+++ inventory_backend/prisma/migrations/20251219092322_init_bulk_transaction/migration.sql
@@ -28,20 +28,31 @@ CREATE TABLE `Item` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `StockTransaction` (
+CREATE TABLE `Transaction` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `code` VARCHAR(191) NOT NULL,
     `type` ENUM('IN', 'OUT') NOT NULL,
-    `quantity` INTEGER NOT NULL,
-    `status` ENUM('PENDING', 'APPROVED', 'REJECTED') NOT NULL DEFAULT 'APPROVED',
-    `approvedById` INTEGER NULL,
+    `status` ENUM('PENDING', 'APPROVED', 'REJECTED', 'COMPLETED') NOT NULL,
     `documentNo` VARCHAR(191) NOT NULL,
     `externalParty` VARCHAR(191) NULL,
-    `notes` VARCHAR(191) NULL,
+    `notes` TEXT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `itemId` INTEGER NOT NULL,
+    `updatedAt` DATETIME(3) NOT NULL,
     `userId` INTEGER NOT NULL,
+    `approvedById` INTEGER NULL,
 
-    UNIQUE INDEX `StockTransaction_documentNo_key`(`documentNo`),
+    UNIQUE INDEX `Transaction_code_key`(`code`),
+    UNIQUE INDEX `Transaction_documentNo_key`(`documentNo`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `TransactionItem` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `transactionId` INTEGER NOT NULL,
+    `itemId` INTEGER NOT NULL,
+    `quantity` INTEGER NOT NULL,
+
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -49,10 +60,13 @@ CREATE TABLE `StockTransaction` (
 ALTER TABLE `Item` ADD CONSTRAINT `Item_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `StockTransaction` ADD CONSTRAINT `StockTransaction_itemId_fkey` FOREIGN KEY (`itemId`) REFERENCES `Item`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Transaction` ADD CONSTRAINT `Transaction_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `StockTransaction` ADD CONSTRAINT `StockTransaction_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Transaction` ADD CONSTRAINT `Transaction_approvedById_fkey` FOREIGN KEY (`approvedById`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `StockTransaction` ADD CONSTRAINT `StockTransaction_approvedById_fkey` FOREIGN KEY (`approvedById`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `TransactionItem` ADD CONSTRAINT `TransactionItem_transactionId_fkey` FOREIGN KEY (`transactionId`) REFERENCES `Transaction`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `TransactionItem` ADD CONSTRAINT `TransactionItem_itemId_fkey` FOREIGN KEY (`itemId`) REFERENCES `Item`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
